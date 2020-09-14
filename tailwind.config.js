2e72cdf3fd79b4f6690d28e27fe36f3c4fb6e4de
Sean Doyle <sean.p.doyle24@gmail.com>
2020-09-13 21:45:18 -0400

    Style with TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          lighter: "#988e83",
+          default: "#574436",
+        },
+        clementine: {
+          lighter: "#ed9a80",
+          default: "#fc5520",
+          darker: "#f8ded6",
+        },
+        cream: {
+          lighter: "#f7f3ed",
+          default: "#f5efe7",
+          darker: "#ecdbc6",
+        }
+      }
+    }
+  }
+}
