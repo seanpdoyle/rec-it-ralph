73a4a52aa47ba34788059c759beddf7bb5ba29bb
Sean Doyle <sean.p.doyle24@gmail.com>
2020-09-16 23:27:15 -0400

    Wire up Rec-It-Ralph-ios

--- ios/rec-it-ralph/rec-it-ralph/SceneDelegate.swift
+++ ios/rec-it-ralph/rec-it-ralph/SceneDelegate.swift
@@ -1,28 +1,53 @@
-import UIKit
 import SwiftUI
+import Turbolinks
+import UIKit
+import WebKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var applicationController = ApplicationViewController()
+    lazy var turbolinksSession: Session = {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "RecItRalph iOS"
+        
+        return Session(webViewConfiguration: configuration)
+    }()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            
+            window.rootViewController = applicationController
+            turbolinksSession.delegate = self
+            visit(URL(string: "http://localhost:3000")!, animated: false)
+            
             self.window = window
             window.makeKeyAndVisible()
         }
     }
-
-
+    
+    func visit(_ URL: URL, animated: Bool) {
+        let visitable = VisitableViewController(url: URL)
+        applicationController.pushViewController(visitable, animated: animated)
+        
+        turbolinksSession.visit(visitable)
+    }
 }
 
+extension SceneDelegate: SessionDelegate {
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        // TODO
+    }
+    
+    func session(_ session: Session, didProposeVisitToURL URL: URL, withAction action: Action) {
+        switch action {
+        case .Replace:
+            visit(URL, animated: false)
+        default:
+            visit(URL, animated: true)
+        }
+    }
+    
+}
