38e548149778cdb45e0006f52386472ac62597fc
Sean Doyle <sean.p.doyle24@gmail.com>
2020-09-16 23:28:46 -0400

    Add iOS platform-specific styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action { request.variant = :ios if platform.ios_app? }
+
+  private
+
+  helper_method def platform
+    @platform ||= ApplicationPlatform.new(request.user_agent)
+  end
 end
